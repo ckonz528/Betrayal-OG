7555671e297c3f1127225252b81f603732dcdef5
Claire Konz <claire.konz@gmail.com>
2021-07-10 11:58:57 -0400

    change floors & place tiles for that floor

--- game/betrayal.py
+++ game/betrayal.py
@@ -17,20 +17,29 @@ class Betrayal:
             room_info) for room_info in tiles}
 
         # create floors
-        '''make a dictionary of floors
-           have a string that tracks current floor (self.current_floor)
-           replace self.ground with self.floors['ground'] or self.floors[self.current_floor]
-           place starting tiles
-           add logic for keys to switch floors (b,g,u)
-        '''
-        self.ground = Gameboard()
-        start_tiles = ['Entrance Hall', 'Foyer', 'Grand Staircase']
-        x = 4
-        for room in start_tiles:
-            self.ground.place_tile(self.rooms[room], (x, 1))
-            x -= 1
+        self.floors = {'ground': Gameboard(), 'basement': Gameboard(),
+                       'upper': Gameboard()}
+
+        # add starting tiles
+        start_tiles = [(4, 'Entrance Hall'), (3, 'Foyer'),
+                       (2, 'Grand Staircase')]
+        for x, room in start_tiles:
+            self.floors['ground'].place_tile(self.rooms[room], (x, 1))
             del self.rooms[room]
-        self.ground.recent_pos = None
+
+        self.floors['upper'].place_tile(self.rooms['Upper Landing'], (2, 1))
+        del self.rooms['Upper Landing']
+
+        self.floors['basement'].place_tile(
+            self.rooms['Basement Landing'], (2, 1))
+        del self.rooms['Basement Landing']
+
+        # disable rotation for all floors
+        for floor in list(self.floors.keys()):
+            self.floors[floor].recent_pos = None
+
+        # set current floor
+        self.current_floor = 'ground'
 
         # shuffle list of tiles
         self.room_keys: list[str] = list(self.rooms.keys())
@@ -56,7 +65,7 @@ class Betrayal:
                         # TODO: add log for messages
                         print('No more tiles available for this floor.')
                     else:
-                        self.ground.place_tile(
+                        self.floors[self.current_floor].place_tile(
                             room, (mouse_x // s.TILE_SIZE + self.camera[0], mouse_y // s.TILE_SIZE + self.camera[1]))
                         redraw_needed = True
 
@@ -83,23 +92,45 @@ class Betrayal:
 
                     # rotate tiles
                     elif keys[pygame.K_n]:
-                        self.ground.rotate_recent('left')
+                        self.floors[self.current_floor].rotate_recent('left')
                         redraw_needed = True
                     elif keys[pygame.K_m]:
-                        self.ground.rotate_recent('right')
+                        self.floors[self.current_floor].rotate_recent('right')
                         redraw_needed = True
+
+                    # switch floors
+                    elif keys[pygame.K_u]:
+                        self.current_floor = 'upper'
+                        self.camera = (0, 0)
+                        redraw_needed = True
+                    elif keys[pygame.K_g]:
+                        self.current_floor = 'ground'
+                        self.camera = (0, 0)
+                        redraw_needed = True
+                    elif keys[pygame.K_b]:
+                        self.current_floor = 'basement'
+                        self.camera = (0, 0)
+                        redraw_needed = True
+
             if redraw_needed:
                 self.redraw()
         pygame.quit()
 
     def redraw(self):
         self.screen.fill(s.WHITE)
-        self.ground.draw_board(self.screen, self.camera)
+        self.floors[self.current_floor].draw_board(self.screen, self.camera)
         pygame.display.flip()
 
     def pick_room(self):
+        if self.current_floor == 'basement':
+            floor = 1
+        elif self.current_floor == 'ground':
+            floor = 2
+        elif self.current_floor == 'upper':
+            floor = 4
+
         for i, room_name in enumerate(self.room_keys):
-            if self.rooms[room_name].floors == 2:
+            if self.rooms[room_name].floors & floor:
                 room = self.rooms[room_name]
                 del self.room_keys[i]
                 return room
