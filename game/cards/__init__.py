4d0b9d0d1fdc18dadc58cc723c2405ee2ca80f69
Raghav Malik <maliragh@amazon.com>
2021-07-29 18:12:24 -0500

    reworked item class

--- game/cards/__init__.py
+++ game/cards/__init__.py
@@ -1,6 +1,6 @@
 class Item:
-    def __init__(self):
-        self.name = None
+    def __init__(self, game_info):
+        self.game_info = game_info
 
     def on_acquire(self, player):
         raise NotImplemented
@@ -10,3 +10,33 @@ class Item:
 
     def on_lose(self, player):
         raise Exception(f'{self.name} cannot be dropped, traded, or stolen')
+
+    def usable(self):
+        try:
+            self.on_use(None)
+        except Exception as e:
+            if 'not a usable item' in str(e):
+                return False
+            return True
+
+    def losable(self):
+        try:
+            self.on_use(None)
+        except Exception as e:
+            if 'cannot be dropped' in str(e):
+                return False
+            return True
+
+
+def name(n):
+    def decorate(cls):
+        old_init = cls.__init__
+
+        def new_init(self, *args, **kwargs):
+            self.name = n
+            old_init(self, *args, **kwargs)
+
+        cls.__init__ = new_init
+        return cls
+
+    return decorate
--- game/cards/omens.py
+++ game/cards/omens.py
@@ -1,13 +1,11 @@
-from . import Item
+from . import Item, name
 
 
+@name("boobs")
 class Book(Item):
-    def __init__(self):
-        self.name = "Book"
-        self.droppable = True
-
     def on_acquire(self, player):
-        player.raise_stat('knowledge', 2)
+        print('lowering knowledge by 2')
+        # player.raise_stat('knowledge', 2)
 
     def on_lose(self, player):
         player.lower_stat('knowledge', 2)
